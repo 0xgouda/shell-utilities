71206fef5c00effcedd4cb7ee95e65e0030958e5
Ahmed Gouda <119757067+0xgouda@users.noreply.github.com>
2024-11-02 06:23:37 +0200

    unzip command

---
+++ gunzip/gunzip.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+    {
+        printf("wunzip: file1 [file2 ...]\n");
+        return 1;
+    }
+
+    for (int i = 1; i < argc; i++)
+    {
+        FILE *file = fopen(argv[1], "rb");
+
+        char buffer;
+        int number;
+        while (fread(&number, sizeof(int), 1, file) && 
+                fread(&buffer, sizeof(char), 1, file))
+        {
+
+            for (int i = 0; i < number; i++)
+            {
+                printf("%c", buffer);
+            }
+            printf("\n");
+        }
+        fclose(file);
+    }
+}
