6e07bb668e4b96a3ef114189dff7be950f58efca
0xgouda <ahmedabdelmonaem@std.mans.edu.eg>
2024-12-27 09:06:51 +0200

    Handled threads output ordering

--- gpzip/gpzip.c
+++ gpzip/gpzip.c
@@ -6,26 +6,42 @@
 #include <pthread.h>
 #include <stdlib.h>
 
-// TODO: many global vars
+typedef struct {
+    int num;
+    char letter;
+} element;
+
+// TODO: many global vars, return or exit(1), concatenation
 int curr_file = 1;
 int numOfFiles;
 char ** args;
 pthread_mutex_t curr_file_lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t write_lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t write_wait = PTHREAD_COND_INITIALIZER;
 char *ptr = NULL;
 struct stat filestat;
 size_t curr_chunk;
 int WORK_SIZE = 1048576;
+int numOfThreads;
+int current_writer = 0;
 
-void worker() {
+void *worker(void *my_thread_id) {
+    element *my_buffer = (element *) malloc(WORK_SIZE * sizeof(element));
+    if (my_buffer == NULL) {
+        printf("Error Allocating Memory for thread %d", *(int *)my_thread_id);
+        exit(1);
+    }
+
     while (1) {
-        int my_chunk = -1, my_limit = -1;
+        int my_chunk = -1, my_limit = -1, writer_id;
         char *local_ptr = NULL;
         size_t sz;
 
         pthread_mutex_lock(&curr_file_lock);
         if (numOfFiles <= 0) {
             pthread_mutex_unlock(&curr_file_lock);
-            return;
+            free(my_buffer);
+            return NULL;
         }
 
         if (ptr == NULL) {
@@ -39,7 +55,7 @@ void worker() {
 
 
             if (fstat(fd, &filestat) == -1) {
-                printf("Error retriving state of file: %s", args[curr_file]);
+                printf("Error retrieving state of file: %s", args[curr_file]);
                 exit(1);
             }
 
@@ -69,8 +85,11 @@ void worker() {
         }
 
         local_ptr = ptr; sz = filestat.st_size;
+        writer_id = current_writer;
         pthread_mutex_unlock(&curr_file_lock);
 
+
+        int buffer_len = 0;
 
         int counter = 1; char current_char = '\0';
         int first_char = 1;
@@ -81,11 +100,23 @@ void worker() {
             } else if (local_ptr[i] == current_char) {
                 counter++;
             } else {
-                fwrite(&counter, sizeof(int), 1, stdout);
-                fwrite(&current_char, sizeof(char), 1, stdout);
+                my_buffer[buffer_len].num = counter;
+                my_buffer[buffer_len].letter = current_char;
+                buffer_len++;
                 counter = 1; current_char = local_ptr[i];
             }
         }
+
+        pthread_mutex_lock(&write_lock);
+        while (current_writer != writer_id) {
+            pthread_cond_wait(&write_wait, &write_lock);
+        }
+
+        fwrite(my_buffer, sizeof(element), buffer_len, stdout);
+        current_writer = (current_writer + 1) % numOfThreads;
+
+        pthread_cond_signal(&write_wait);
+        pthread_mutex_unlock(&write_lock);
 
         if (my_chunk + my_limit >= sz) {
             munmap(local_ptr, sz);
@@ -95,20 +126,23 @@ void worker() {
 
 int main (int argc, char** argv) {
     if (argc == 1) {
-        printf("gzip: file1 [file2 ...]\n");
+        printf("gpzip: file1 [file2 ...]\n");
         return 1;
     }
 
     numOfFiles = argc - 1;
     args = argv;
 
-    int numOfCpus = get_nprocs();
-    pthread_t threads[numOfCpus];
-    for (int i = 0; i < numOfCpus; i++) {
-        pthread_create(&threads[i], NULL, (void *) worker, NULL);
+    numOfThreads = get_nprocs();
+    pthread_t threads[numOfThreads];
+    int *tmp = malloc(sizeof(int) * numOfThreads);
+    for (int i = 0; i < numOfThreads; i++) {
+        tmp[i] = i;
+        pthread_create(&threads[i], NULL, worker, &tmp[i]);
     }
 
-    for (int i = 0; i < numOfCpus; i++) {
+    for (int i = 0; i < numOfThreads; i++) {
         pthread_join(threads[i], NULL);
     }
+    free(tmp);
 }
